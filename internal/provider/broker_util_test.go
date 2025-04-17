3bd576cc5d57e302d1294ede859eb2465897f1f5
Hartmut Franz <hartmut.franz@gebit.de>
2025-04-17 09:28:02 +0200

    fix: go mod tidy & cleanup

--- internal/provider/broker_util_test.go
+++ internal/provider/broker_util_test.go
@@ -6,11 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSomething(t *testing.T) {
-	// assert equality
-	assert.Equal(t, 123, 123, "they should be equal")
-}
-
 func TestGetRouterPrefix1(t *testing.T) {
 	// assert equality
 	assert.Equal(t, "test123", getRouterPrefix("test123primarycn"), "primarycn suffix")
